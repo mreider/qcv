f7a423269d2fcf566eacc1cb6bbb2dd53bc54d5c
Jijoy <jijoy21@gmail.com>
2016-08-12 00:06:30 +0530

    Task 2:There should be a button on the home page that says Sign in with LinkedIn , not completed

--- app/hello.py
+++ app/hello.py
@@ -1,25 +1,72 @@
 """ hello.py """
+import json
 import traceback
-
-from flask import Flask, jsonify
-
-from database import Database
+import datetime
+from flask import Flask, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
+import requests
+from config import SETTINGS
+from uuid import uuid4
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://'+SETTINGS['DB_USER_NAME']+':'+SETTINGS['DB_USER_NAME']+'@'+\
+                                    SETTINGS['DB_HOST']+'/'+SETTINGS['DB_NAME']
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
+
+class UserProfile(db.Model):
+    __tablename__ = 'user_profile'
+    user_id = db.Column(db.Integer,primary_key=True)
+    created = db.Column(db.DateTime,default=datetime.datetime.utcnow)
+
+linked_in_url = 'https://www.linkedin.com/oauth/v2/authorization'
+
+@app.route('/sync',methods=['GET','POST'])
+def sync():
+    return session['access_token']
+
+@app.route('/callback',methods=['GET','POST'])
+def auth_callback():
+    code = request.args.get('code')
+    if SETTINGS['ON_DEV']:
+        callback_url = 'http://localhost:5000/callback'
+    else:
+        callback_url = 'http://qcv.space/callback'
+
+    auth_url = 'https://www.linkedin.com/oauth/v2/accessToken'
+    grant_type = 'authorization_code'
+    client_id = SETTINGS['LINKEDIN_CLIENT_ID']
+    client_secret = SETTINGS['LINKEDIN_CLIENT_SECRET']
+
+    params = {'code':code,'grant_type':grant_type,'redirect_uri':callback_url,'client_id':client_id,'client_secret':client_secret}
+
+    response = requests.post(auth_url,data=params)
+    print response.status_code
+    if response.status_code == 200:
+        content = json.loads(response.text)
+        session['access_token'] = content.get('access_token')
+        return render_template('home.html')
+    else:
+        return render_template('error.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if SETTINGS['ON_DEV']:
+        callback_url = 'http://localhost:5000/callback'
+    else:
+        callback_url = 'http://qcv.space/callback'
+    code = str(uuid4).replace('-','')
+    print code
+    url = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id='+SETTINGS['LINKEDIN_CLIENT_ID']+'&redirect_uri='+callback_url+'&state='+code+'&scope=r_basicprofile'
+    return redirect(url,code=302)
 
 @app.route('/')
 def hello():
-    print 'Came here'
-    # db_mgr = Database()
-    try:
-        db_mgr = Database()
-        return jsonify({
-            'hello': 'world'
-        })
-    except:
-        traceback.print_exc()
-    finally:
-        db_mgr.connection_close()
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    db.create_all()
+    app.run(host='0.0.0.0',debug=True)
